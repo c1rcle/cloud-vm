21f114204220487ddc1444661d3c87547e784fb8
Jan Miszczyk <jachuuu_15@hotmail.com>
2022-05-09 01:00:43 +0200

    Added result tagging support for the YCSB benchmark

--- ycsb/client/benchmark.sh
+++ ycsb/client/benchmark.sh
@@ -4,6 +4,7 @@ usage () {
     echo "YCSB benchmark
     -y  [required] Path to YCSB root folder
     -c  [required] MongoDB connection string
+    -n  [required] Test tag
     -r  Database record count
     -t  Worker threads
     -o  Target operations per second"
@@ -14,10 +15,11 @@ record_count=50000
 threads=1
 target=100
 
-while getopts y:c:r:t:o: option; do
+while getopts y:c:n:r:t:o: option; do
     case $option in
         (y) ycsb_root=$OPTARG;;
         (c) mongo_url=$OPTARG;;
+        (n) tag=$OPTARG;;
         (r) record_count=$OPTARG;;
         (t) threads=$OPTARG;;
         (o) target=$OPTARG;;
@@ -25,7 +27,7 @@ while getopts y:c:r:t:o: option; do
     esac
 done
 
-if [ ! "$ycsb_root" ] || [ ! "$mongo_url" ]
+if [ ! "$ycsb_root" ] || [ ! "$mongo_url" ] || [ ! "$tag" ]
 then
    echo "Required arguments not provided"
    usage
@@ -36,7 +38,7 @@ run_benchmark () {
         -p mongodb.url=$mongo_url \
         -p mongodb.upsert=true \
         -p recordcount=$record_count \
-        -s > "results/$2_load_result.txt"
+        -s > "$output_folder/$2_load_result.txt"
 
     ./$ycsb_root/bin/ycsb.sh run mongodb -P $1 \
         -p mongodb.url=$mongo_url \
@@ -44,15 +46,14 @@ run_benchmark () {
         -p measurementtype=timeseries \
         -threads $threads \
         -target $target \
-        -s > "results/$2_run_result.txt"
+        -s > "$output_folder/$2_run_result.txt"
 
     mongosh --quiet "$mongo_url" --eval "db.dropDatabase()"
 }
 
-if [ ! -d results ]
-then
-    mkdir results
-fi
+timestamp=$(date +%Y-%m-%dT%H-%M-%S)
+output_folder="results/$tag/$timestamp"
+mkdir -p $output_folder
 
 # Update-heavy (workload A)
 run_benchmark "$ycsb_root/workloads/workloada" "a"
